0ba71a430c2b6a7036849af93eaa43bcb4025e18
Abubakir <ikrima84@gmail.com>
2024-09-20 15:18:54 +0600

    startproject3

--- beckend/api/paginations.py
+++ beckend/api/paginations.py
@@ -1,5 +1,3 @@
-from math import ceil
-from httplib2 import Response
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -8,13 +6,3 @@ class SimplePagintion(PageNumberPagination):
     page_query_param = 'page'
     page_size_query_param = 'page_size'
     max_page_size = 100
-    def get_paginated_response(self, data):
-        return Response({
-            'count': self.page.paginator.count,  
-            'page_size': self.page.paginator.per_page,  
-            'current_page': self.page.number,  
-            'total_pages': ceil(self.page.paginator.count / self.page.paginator.per_page), 
-            'next': self.get_next_link(), 
-            'previous': self.get_previous_link(),  
-            'results': data  \
-        })
